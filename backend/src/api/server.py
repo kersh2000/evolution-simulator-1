f4839ba059f20b18dc4a1c56e5bc7492d41f42cd
Bradley Kershaw <bradleykershaw333@gmail.com>
2024-03-16 23:41:04 +0000

    Basic API setup

---
+++ backend/src/api/server.py
@@ -0,0 +1,16 @@
+import os
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    # Construct the absolute path
+    home_path = os.path.join(os.path.dirname(__file__), 'templates', 'home.html')
+    return FileResponse(home_path)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv('SERVER_PORT', 8000))  # Default to port 8000 if not specified
+    uvicorn.run(app, host="localhost", port=port)
