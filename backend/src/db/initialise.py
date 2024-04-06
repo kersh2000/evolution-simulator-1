5ff18c427d1938eb7fa3e3542d82d446d5c216b5
Bradley Kershaw <bradleykershaw333@gmail.com>
2024-04-06 11:24:57 +0100

    Remove unnecessary files

--- backend/src/db/initialise.py
+++
@@ -1,9 +0,0 @@
-# db/initialize_db.py
-from db.db import engine
-from models.user import Base  # Import Base from the user_model module
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    create_tables()
