67473bafab6c4120f2053d94ad732c549e71f07e
Bradley Kershaw <bradleykershaw333@gmail.com>
2024-04-07 00:27:56 +0100

    Update to include routers and prevent pycache

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -1,8 +1,28 @@
+import sys
+sys.dont_write_bytecode = True
+
 import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+from ..db.models import models
+from ..db.db import engine
+
+from .routers import routers
+
+
+load_dotenv('.env')
+
+# Creates all of the models for our database
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(routers.authRouter)
+app.include_router(routers.userRouter)
+app.include_router(routers.simulationRouter)
+app.include_router(routers.blockRouter)
 
 @app.get("/")
 async def read_root():
