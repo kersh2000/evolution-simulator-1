eb7025f148916596c1fc05fe319ad02fff5c1bce
Bradley Kershaw <bradleykershaw333@gmail.com>
2024-04-07 00:38:59 +0100

    Remove models index file, replace with file called 'models' instead of 'index'

---
+++ backend/src/db/models/models.py
@@ -0,0 +1,8 @@
+from ..db import Base
+
+# Import all models for ease-of-access
+from .user import User
+from .environment import Environment
+from .dogma import Dogma
+from .block import Block
+from .simulation import Simulation
